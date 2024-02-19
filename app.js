84ab96f5e363bc3a795f3c3783f4bbf34e3a6ea8
Sujana-dev <sujanaa482@gmail.com>
2024-02-20 02:32:55 +0600

    new form added

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
-let inputBox = document.querySelector(".input_box")
+let nameInput = document.querySelector(".name_input")
+let nameOutPut = document.querySelector(".name_output")
+
+let idInput = document.querySelector(".id_input")
+let idOutPut = document.querySelector(".id_output")
+
+let emailInput = document.querySelector(".email_input")
+let emailOutPut = document.querySelector(".email_output")
+
+let no_input = document.querySelector(".no_input")
+let no_output = document.querySelector(".no_output")
 
 let button = document.querySelector(".btn")
 
-let getOutput = document.querySelector(".output")
+button.addEventListener("click",function(){ 
 
-button.addEventListener("click",function(){
-    // getOutput.innerHTML = inputBox.value 
-
-    if(inputBox.value == ""){
-        getOutput.innerHTML = "Please enter your name"
+    if(nameInput.value == ""){
+        nameOutPut.innerHTML = "Please enter your name"
     }
+    else if(idInput.value == ""){
+        idOutPut.innerHTML = "Please enter your id"
+    }
+    else if(emailInput.value == ""){
+        emailOutPut.innerHTML = "Please enter your email"
+    }
+    else if(no_input.value == ""){
+        no_output.innerHTML = "Please enter your phone number"
+    }
+
       else{
-        getOutput.innerHTML = inputBox.value 
+        nameOutPut.innerHTML = nameInput.value 
+        idOutPut.innerHTML = idInput.value
+        emailOutPut.innerHTML = emailInput.value
+        no_output.innerHTML = no_input.value
       }
 })
